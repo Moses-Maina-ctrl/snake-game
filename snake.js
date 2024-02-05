fec75d5975f692106a07f12c10c819bbdd6af73c
Moses-Maina-ctrl <mosesmaina1245@gmail.com>
2024-02-05 15:03:23 +0300

    Create snake function

--- snake.js
+++ snake.js
@@ -3,10 +3,46 @@ const ctx = board.getContext("2d");
 const startText = document.getElementById("start");
 const logo = document.querySelector("#logo")
 
+let xPlayerPosition = 10, yPlayerPosition = 10;
+let gridSize = 20; tileCount = 20;
+let x_dir = 0, y_dir = 0;
+let gameStart = false
+document.addEventListener("keydown", pressKey)
+
 function startGame() {
-  let gameStart = true;
-  document.addEventListener("keydown", pressKey)
-  setInterval(game, 1000 / 15); //milliseconds per framerate(15);
+  gameStart = true;
   startText.style.display = "none";
   logo.style.display = "none";
+  setInterval(game, 1000 / 15); //milliseconds per framerate(15);
+}
+function game() {
+  xPlayerPosition += x_dir;
+  yPlayerPosition += y_dir;
+}
+function pressKey(event) {
+  if (!gameStart && event.keyCode === 32) {
+    startGame();
+    console.log("Space")
+  } else {
+    switch (event.keyCode) {
+      case 37:
+        x_dir = -1; y_dir = 0;
+        console.log("right")
+        break;
+      case 38:
+        x_dir = 0; y_dir = 1;
+        break;
+      case 39:
+        x_dir = -1; y_dir = 0;
+        break;
+      case 40:
+        x_dir = 0; y_dir = -1;
+        break;
+
+
+
+
+
+    }
+  }
 }
